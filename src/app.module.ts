e2841b9e58397efe8840d758b51de7340f5603a2
Artag/Chris <scristxyz@gmail.com>
2024-11-25 16:09:15 -0500

    act instagram

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { InstagramModule } from './instagram/instagram.module';
 
 @Module({
-  imports: [WhatsappModule],
+  imports: [WhatsappModule, InstagramModule],
   controllers: [],
   providers: [],
 })
