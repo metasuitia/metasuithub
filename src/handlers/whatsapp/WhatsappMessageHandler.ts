f0fc4de7d62e37904231a615b8c09f157cccfc4e
Artag/Chris <scristxyz@gmail.com>
2024-11-30 12:55:21 -0500

    act

--- src/handlers/whatsapp/WhatsappMessageHandler.ts
+++ src/handlers/whatsapp/WhatsappMessageHandler.ts
@@ -6,13 +6,17 @@ import { Message } from "src/whatsapp/dto";
 
 @Injectable()
 export class WhatsappMessageHandler{
-
-    //TODO: esta clase se encarga de mirar si estamos en horas laborales
+ 
     //TODO: se encargara de filtrar los mensajes
-    //TODO: se encargara de responder mensajes basicos
-    //TODO: se encargara de la comunicacion con nast
     //pasemosle el  array de mensajes y que de aqui el mire las horas 
     //y que tipo de mensaje es
+
+    //TODO: si no estamos en horas laborales igual enviara a nats 
+    //1 se categorizara el mensaje 
+    //2 se envia a nats
+    //3 si el bot puede contestar algo facil lo hara se guardara
+    //4 el hub es el sitio donde se envian mensajes asi que este respondera
+    
     constructor(
         private readonly whatsappMessage: Message, 
         
@@ -24,6 +28,7 @@ export class WhatsappMessageHandler{
     isworkingHours(){
         //aqui ira la  logica de si estamos en horario 
         if (WorkingHours()) {
+            
             console.log(`se enviara el evento al nast ${this.whatsappMessage}`);
             
             return true;
