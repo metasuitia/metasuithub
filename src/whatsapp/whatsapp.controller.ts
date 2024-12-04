9ba225af22ceebf6fedb616142794d30be6da73a
Artag/Chris <scristxyz@gmail.com>
2024-12-04 09:18:48 -0500

    act

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -6,6 +6,11 @@ import { MetaWhatsappMessageDTO, webhookMessageWhatsappDto } from './dto';
 
 @Controller('whatsapp')
 export class WhatsappController {
+  /**********************************************************************
+  Hibrido de microservicios y Rest api  aqui recibimos los mensajes
+  del whatsapp por webhook y los enviamos a los microservicios
+  y aqui recibire mensajes de los mircoservicios y los enviamos a whatsapp
+  ***********************************************************************/
   constructor(private readonly whatsappService: WhatsappService) {}
 
   @Post("/webhook")
@@ -19,7 +24,6 @@ export class WhatsappController {
   gethook(@Query() req:Request,res:Response) {
     const response =this.whatsappService.hooked(req,res)
     return response
-    
     
   }
 
